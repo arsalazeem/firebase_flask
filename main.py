1cefc9fab6a0dcd919d25c7f3d96d11a079030a7
arsalazeem <arsal.azeem@vizteck.com>
2021-03-19 18:47:10 +0500

    firbase basic config and creating a single user

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 #source flaskapi/bin/activate
+import pyrebase
 
 
-
+firebaeconfig={
+  "apiKey": "AIzaSyCVqajMEZRVDPABKg0G0wbcKTZC9rCpC6A",
+  "authDomain": "even-ivy-256205.firebaseapp.com",
+  "databaseURL": "https://even-ivy-256205.firebaseio.com",
+  "projectId": "even-ivy-256205",
+  "storageBucket": "even-ivy-256205.appspot.com",
+  "messagingSenderId": "304328648235",
+  "appId": "1:304328648235:web:b4c5ea61691b9a0750ec6e",
+  "measurementId": "G-45ZKEK5R81"
+}
+try:
+    firbase = pyrebase.initialize_app(firebaeconfig)
+    # db=firbase.database()
+    auth=firbase.auth()
+    auth.sign_in_with_email_and_password("azeemarsal@gmail.com","12345678")
+except Exception as e:
+    print(e)
